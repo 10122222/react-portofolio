43455f63272ade3ed9056cf87311466e1661ed97
10122222 <121574317+10122222@users.noreply.github.com>
2024-02-29 12:36:15 +0000

    Update package.json and component files
    Change homepage URL in package.json and update image paths in component files.

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Muhammad Irkham Nurmauludifa",
+  name: "Muhammad Irkham N",
   title: "Undergraduate Student",
   email: "irkham.10122222@mahasiswa.unikom.ac.id",
   gitHub: "10122222",
